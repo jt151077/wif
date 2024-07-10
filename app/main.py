b78e258dbe8abd16ce4f81d1637e4476522f449c
jt151077 <jt151077@gmail.com>
2024-07-10 12:44:22 +0000

    linting

--- app/main.py
+++ app/main.py
@@ -12,4 +12,4 @@ def runservice1():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 80)))
